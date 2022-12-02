4344cb1c595e29bdb0c0b0a57a5bdfb2cc003d74
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-12-02 09:42:57 -0500

    Added submit to network function.

--- DAO/asset_creation.js
+++ DAO/asset_creation.js
@@ -4,3 +4,17 @@ const algodClient = new algosdk.Algodv2(process.env.ALGOD_TOKEN, process.env.ALG
 
 const creator = algosdk.mnemonicToSecretKey(process.env.MNEMONIC_CREATOR);
 const receiver = algosdk.mnemonicToSecretKey(process.env.MNEMONIC_RECEIVER);
+
+const submitToNetwork = async (signedTxn) => {
+    // send txn
+    let tx = await algodClient.sendRawTransaction(signedTxn).do();
+    console.log("Transaction : " + tx.txId);
+
+    // Wait for transaction to be confirmed
+    confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 4);
+
+    //Get the completed Transaction
+    console.log("Transaction " + tx.txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+
+    return confirmedTxn;
+};
