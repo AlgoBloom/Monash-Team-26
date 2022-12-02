4b78811daca4cba51fcfd8eb39dfe0b59119c740
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-12-02 09:33:13 -0500

    Added class to hold marketplace variables.

--- Marketplace/marketplace.py
+++ Marketplace/marketplace.py
@@ -1,3 +1,14 @@
+# each product is represented by an application
+# buy product by making a call to the app and performing a payment to the creator
+
 from pyteal import *
 
 class Item:
+    # class holds the variables
+    class Vars:
+        supplier = Bytes("Supplier")
+        name = Bytes("Name")
+        image = Bytes("Image")
+        description = Bytes("Description")
+        price = Bytes("Price")
+        sold = Bytes("Sold")
